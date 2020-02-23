ef0e2d9d09a1927869517a1ebe49da53e7bde53b
period9149 <period9149@period9149deAir.lan>
2020-02-23 17:09:45 +0800

    ,

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -48,7 +48,7 @@ module.exports = app => {
     const upload = multer({ dest: __dirname + '../../../uploads' })
     app.post('/admin/api/upload',upload.single('file'), async (req, res) => {
         const file = req.file
-        file.url = `http://localhost:3000/uploads/${file.filename}`
+        file.url = `http://hzau.xxtiyubu.club/uploads/${file.filename}`
         res.send(file)
     })
 
